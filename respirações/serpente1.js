ba0c866832d3bb3eafb15c217a12ed23bb1ce7e8
Diogo A <44744872+JakDaxe@users.noreply.github.com>
2023-02-14 17:19:18 +0000

    Separate folder to avoid mess

---
+++ respirações/serpente1.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+
+exports.run = async (client, message, args) => {
+
+    const serpente = message.guild.roles.cache.find(r => r.name === "⁀➷〘🐍〙• Respiração da Serpente");
+    
+    const embed = new Discord.MessageEmbed()
+      .setColor('#3E005A')
+      .setDescription(`${message.author} usou a ${serpente}\n\n**Primeira Forma: Corte da Serpente Sinuosa (Ichi no kata: Idagiri)**`)
+      .setImage("https://media.discordapp.net/attachments/1072244257282338958/1075100288526073986/d1a65a9b02811f324371b031756e7ef6b97adcafr1-1256-870v2_hq.jpg?width=688&height=476")
+      .setTimestamp()
+     
+     message.channel.send(embed)
+  
+};
+
+
+exports.help = {
+  name: "serpente1",
+  description: "usa a forma 1 da respiração da serpente."
+};
