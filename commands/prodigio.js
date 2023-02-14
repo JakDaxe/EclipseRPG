769ce61337295d45b07165994d05fbc52fcf9e3d
Diogo A <44744872+JakDaxe@users.noreply.github.com>
2023-02-14 15:54:28 +0000

    New command

---
+++ commands/prodigio.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+
+exports.run = async (client, message, args) => {
+
+    //puxar role
+    const ambidestro = message.guild.roles.cache.find(r => r.name === "⁀➷〘⚔️〙• Ambidestro");
+    
+    //resultados
+    const prodigio = [
+        { prod: `🌟 **Você é um prodigio!**\n\nProdígios são aqueles que nasceram com uma habilidade tão grande que facilmente conseguem usar técnicas de respiração/kekkijutsu e adquirirem sentidos Aguçados ou até mesmo outras habilidades.\n\n**Benefícios**: Obtém +25% dos ganhos em missões.\nGanha +2% em treinos\nSobe de Rank com -50% dos requisitos\n-50% do Cooldown de todos os treinos\nComeça com ${ambidestro}`},
+        {
+          prod: `**Você não é um prodígio!**\n\nVocê nasceu como uma pessoa qualquer, e infelizmente você não tem muitas habilidades únicas como um prodigio, mas não desanime você ainda pode evoluir muito!`
+        }
+    ]
+    
+    //sistema de rolar
+    const user = message.mentions.users.first()
+    const randomIndex = Math.floor(Math.random() * prodigio.length);
+    const randomReward = prodigio[randomIndex];
+    
+
+    
+    const embed = new Discord.MessageEmbed()
+      .setColor('#3E005A')
+      .setDescription(`${message.author}, ${randomReward.prod}`)
+      .setTimestamp()
+     
+     
+  
+};
+
+
+exports.help = {
+  name: "prodigio",
+  description: "rolar prodigio."
+};
