1a08a086dc879f805fbc781039f2e693705d82ef
Diogo A <44744872+JakDaxe@users.noreply.github.com>
2023-02-18 15:11:22 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
       .setColor('#3E005A')
       .setTitle("Menu de Ajuda")
-      .setDescription(`${message.author}, Aqui estão algumas informações sobre mim.\n\n**Versão:** \`1.3\`\n**Comandos:** \`18\`\n**Jogadores:** \`${client.users.cache.size}\`\n\n**Lista de Comandos:**\n\`perfil, status, sangue, prodigio, familia, addpoints, removepoints, apv, rpv, obsrpv, dis [atributo], att raça, att aparência, att kekkijutsu, att respiração, add [atributo],\``)
+      .setDescription(`${message.author}, Aqui estão algumas informações sobre mim.\n\n**Versão:** \`1.3\`\n**Comandos:** \`18\`\n**Jogadores:** \`${client.users.cache.size}\`\n\n**Lista de Comandos:**\n\`perfil, status, sangue, prodigio, familia, addpoints, removepoints, apv, rpv, obsrpv, dis [atributo], att raça, att aparência, att kekkijutsu, att respiração, add [atributo], roll\``)
       .setTimestamp()
      
      message.channel.send(embed)
